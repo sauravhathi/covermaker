f3c60eb9b915005b1d469aa0f9f293fbd88d56b7
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-01-31 02:12:53 +0530

    update unsplash image view

--- components/UnsplashSearch.tsx
+++ components/UnsplashSearch.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import { createApi } from "unsplash-js";
 import Image from "next/image";
+import { Link } from "react-router-dom";
 
 export default function UnsplashSearch(props: { defaultData: any, setDefaultData: any }) {
 
     const [searchQuery, setSearchQuery] = useState('');
 
     // image list from api call result
-    const [images, setImages] = useState<string[]>([]);
+    const [images, setImages] = useState<any>([]);
 
     // loading state for image blur effect
     const [isLoading, setLoading] = useState(true)
@@ -33,7 +34,7 @@ export default function UnsplashSearch(props: { defaultData: any, setDefaultData
                     console.log('error occurred: ', result.errors[0]);
                     return;
                 }
-                setImages(result.response.results.map((img: any) => img.urls.regular));
+                setImages(result.response.results);
             });
         } else {
             setImages([]);
@@ -57,32 +58,52 @@ export default function UnsplashSearch(props: { defaultData: any, setDefaultData
                 onChange={e => setSearchQuery(e.target.value)}
                 placeholder="Search Image from Unsplash"
             />
+            {/* searched image list */}
             <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
                 {
                     images.map((item: any, index: number) => {
                         return (
-                            <div key={index} className="w-full overflow-hidden aspect-w-16 aspect-h-9 rounded-sm relative">
-                                <Image
-                                    src={item}
-                                    alt="unsplash"
-                                    fill
-                                    sizes="true"
-                                    className={` object-cover select-none ${isLoading ? 'blur-2xl grayscale' : 'blur-0 grayscale-0'} transition-all duration-500`}
-                                    onClick={() => props.setDefaultData([
-                                        {
-                                            ...props.defaultData[0],
-                                            cover: item
-                                        }
-                                    ])}
-                                    onLoadingComplete={() => setLoading(false)}
-                                    onDoubleClick={() => handleDownload(item)}
-                                />
-                            </div>
+                            <>
+                                <div className="">
+                                    <div key={index} className="w-full overflow-hidden aspect-w-16 aspect-h-9 relative rounded-t-md">
+                                        <Image
+                                            src={item.urls.regular}
+                                            alt="unsplash"
+                                            fill
+                                            sizes="true"
+                                            className={` object-cover select-none ${isLoading ? 'blur-2xl grayscale' : 'blur-0 grayscale-0'} transition-all duration-500`}
+                                            onClick={() => props.setDefaultData([
+                                                {
+                                                    ...props.defaultData[0],
+                                                    cover: item.urls.regular,
+                                                }
+                                            ])}
+                                            onLoadingComplete={() => setLoading(false)}
+                                            onDoubleClick={() => handleDownload(item.links.html)}
+                                        />
+                                    </div>
+                                    <div className="p-2 bg-white shadow-sm rounded-b-md">
+                                        <p className="text-sm text-gray-500">
+                                            Photo by{' '}
+                                            <a href={item.user.links.html + "?utm_source=https://covermaker.vercel.app/&utm_medium=referral"}
+                                                target="_blank" rel="noreferrer" className="text-blue-500 font-semibold hover:underline">
+                                                {item.user.name}
+                                            </a>
+                                            {' '}
+                                            on
+                                            {' '}
+                                            <a href="https://unsplash.com/?utm_source=https://covermaker.vercel.app/&utm_medium=referral" target="_blank" rel="noreferrer" className="text-blue-500 font-semibold hover:underline">
+                                                Unsplash
+                                            </a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </>
                         )
                     })
                 }
             </div>
-        </div>
+        </div >
     )
 }
 
